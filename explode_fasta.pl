e9af57c8fccb65036b8ac3610c3a282208240ab2
srividya <srividya.ramki@gmail.com>
2017-05-30 16:12:55 -0400

    updated script to chunk 100 contig fasta sequences if number of sequences are > 100

--- explode_fasta.pl
+++ explode_fasta.pl
@@ -1,35 +1,93 @@
-#!/usr/bin/perl -w
+#!/opt/hpc/pkg/perl-5.14/bin/perl -w
 use strict;
 
 #initialling variables:
 my $fh = undef;
+my $seqs_per_file = 100;
+my $file_number = 1;
 my $curseq = undef;
 my $filename = join('',$ARGV[1],"/summary");
 my $name=undef;
-
 #open files:
 open (my $summary, ">", $filename) || die "Could not open file '$filename: $!\n";
 
 #Fasta file:
 open(FILE1, $ARGV[0]) || die "Could not open file '$ARGV[0]: $!\n";
+my $count = 0;
 
-
-
-while (<FILE1>) #reads in line after line
-{
-  if (/>(\S+)/) #If fasta header:
-  {
-    $curseq = $1;
-    print STDERR "\tSplitting $curseq\n";
-    $name=join('',$ARGV[1],"/$curseq.fa");
-    open $fh, ">",$name or die "Cant open $curseq.fa ($!)\n";
-
-    print $fh ">$curseq\n"; #print heaer in new file
-	
-    print $summary "$name\n"; #adds the name of the file to the summary file
-  }
-  else #sequence:
-  {
-    print $fh $_;	
-  }
+while( <FILE1> ){
+    chomp $_;
+       if($_=~ /^>/g ) 
+       {
+             $count++; 
+    }
 }
+
+print STDERR "Total number of sequences : $count\n";
+seek FILE1, 0, 0;
+my $seq_ctr = 0;
+## If the number of sequneces is less than 100 then split to individual contigs
+if($count < 100) {
+  while (<FILE1>) #reads in line after line
+  {
+  	if (/>(\S+)/) #If fasta header:
+        {
+        $curseq = $1;
+        print STDERR "\tSplitting $curseq\n";
+        $name=join('',$ARGV[1],"/$curseq.fa");
+        open $fh, ">",$name or die "Cant open $curseq.fa ($!)\n";
+        print $fh ">$curseq\n"; #print heaer in new file
+        print $summary "$name\n"; #adds the name of the file to the summary file
+        }  
+        else{
+           print $fh $_;	
+        }
+  } 
+ }
+else{ #If the number of sequneces is greater than 100 then split to chunks of 100 sequences
+   while (<FILE1>) #reads in line after line
+   {
+        if (/>(\S+)/) {  #If fasta header:
+ 
+        if ( $seq_ctr++ % $seqs_per_file == 0) {
+        my @arr=split /(\d+)/, $1;
+        my $seq_base=$arr[0];
+        my $end_contig = $seq_ctr + 99;
+        if ( $end_contig  > $count ){ $end_contig = $count; }  
+        $curseq = $seq_base .$seq_ctr. "-" . $end_contig;
+        $name=join('',$ARGV[1],"/$curseq.fa");
+        #close($fh);
+    	open $fh, ">",$name or die "Cant open $curseq.fa ($!)\n";
+
+    	#print $fh ">$curseq\n"; #print heaer in new file
+	
+    	print $summary "$name\n"; #adds the name of the file to the summary file
+        }
+        }
+        print $fh $_;
+    }	
+} 
+#
+#my $fasta_file = "something.fasta";
+#my $seqs_per_file = 100;  # whatever your batch size
+#
+#my $file_number = 1;  # our files will be named like "something.fasta.1"
+#my $seq_ctr = 0;
+#
+#open(FASTA, $fasta_file) || die("can't open $fasta_file");
+#
+#while(<FASTA>) {
+#
+#    if(/^>/) {
+#
+#           # open a new file if we've printed enough to one file
+#                  if($seq_ctr++ % $seqs_per_file == 0) {
+#                           close(OUT);
+#                                    open(OUT, "> " . $fasta_file . "." . $file_number++);
+#                                           }
+#
+#                                               }
+#
+#                                                   print OUT $_;
+#
+#                                                    }
